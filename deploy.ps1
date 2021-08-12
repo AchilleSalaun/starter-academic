a59761253890fb86d860925a9fbf17f42fdbbab2
AchilleSalaun <achillesalaun@gmail.com>
2021-08-12 15:27:35 +0200

    get date into commit (deploy)

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,8 @@
 D: ;
 cd .\Achille\Travail\Git\page_perso\ ;
+$date = Get-Date
 hugo ;
 cd .\public\ ;
 git add . ;
-git commit -m "Deploy" ;
+git commit -m "Deploy [$date]" ;
 git push origin master ;
